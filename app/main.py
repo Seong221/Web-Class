3acac6a6657027c7b1aa275a6fb29d4fe9c321aa
Seong221 <anjdla2111@gmail.com>
2024-04-19 15:53:55 +0900

    WebClass

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
-from typing import Union
-
 from fastapi import FastAPI
+
+import requests
 
 app=FastAPI()
 
+
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def root():
+    URL="https://bigdata.kepco.co.kr/openapi/v1/powerUsage/industryType.do?year=2020&month=11&metroCd=11&cityCD=12&apiKey=cu3z69qA0ce88cnS5wI8K0QJRoe3tTA3OVs641U5&returnType=json"
+    
 
-@app.get("/items/{item_id}")
-def read_item(item_id : int, q: Union[str, None]=None):
-    return {"item_id" : item_id, "q" : q}
+    contents=requests.get(URL).text
 
+    return {"message":contents}
 
